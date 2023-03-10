1c9373386a1c266642d29c15b6de77b8699c962f
hishammfaume <hishammfaume@gmail.com>
2023-03-10 20:43:10 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    nums = len(args)
+    if nums == 0:
+        print("0 arguments.")
+    elif nums == 1:
+        print("1 argument:")
+    else:
+        print(f"{nums} arguments:")
+    for i, arg in enumerate(args):
+        i += 1
+        print("{}: {}".format(i, arg))
