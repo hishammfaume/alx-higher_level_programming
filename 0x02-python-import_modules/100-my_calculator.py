23771e17e7331d6bf898ecc22a5057625963848c
hishammfaume <hishammfaume@gmail.com>
2023-03-11 10:21:32 +0000

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,13 +2,12 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, div, mul
-    
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
     op = sys.argv[2]
-    b =int(sys.argv[3])
+    b = int(sys.argv[3])
     if op == "+":
         result = add(a, b)
         op_str = "+"
