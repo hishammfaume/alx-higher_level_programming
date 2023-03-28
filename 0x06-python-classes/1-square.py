b420a0e3b93b3a2fd75665877511ec1973a941b3
hishammfaume <hishammfaume@gmail.com>
2023-03-28 15:18:05 +0000

    private instantation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a class that defines a square"""
+
+
+class Square:
+    """private instance of size"""
+
+    def __init__(self, size):
+        """instantation with size"""
+
+        self.__size = size
