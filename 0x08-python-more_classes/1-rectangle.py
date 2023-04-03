64731b309bc51ea485d7ff670ed485b131cba49e
hishammfaume <hishammfaume@gmail.com>
2023-04-03 20:07:44 +0000

    setters and getters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,17 +9,17 @@ class Rectangle():
         self.__height = height
         self.__width = width
 
-    """defining a width property"""
 
     @property
     def width(self):
+        """getter for private instance attribute width"""
         return self.__width
 
-    """property setter"""
+
 
     @width.setter
     def width(self, value):
-        """checking if width is an integer and greater than 0"""
+        """setter for private instance attribute width"""
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -27,15 +27,15 @@ class Rectangle():
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    """defining height property to retrieve it"""
     @property
     def height(self):
+        """getter for the private instance attribute height"""
         return self.__height
 
-    """height property setter"""
+    
     @height.setter
     def height(self, value):
-        """checkin if height is an integer and greater than 0"""
+        """setter for priate instance attribute height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
