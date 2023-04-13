8c607ebc12a8b1c4083f6dd379eb7e6d8036733f
hishammfaume <hishammfaume@gmail.com>
2023-04-13 11:20:46 +0000

    validator

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,34 +1,17 @@
 #!/usr/bin/pyhton3
 """class inheritance"""
 
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
-class BaseGeometry():
-    """public instance"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validating the value"""
-
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
-        if value <= 0:
-            raise ValueError("<name> must be greater than 0")
-
-
-"""creating a new class"""
-
+"""rectangle rep"""
 
 class Rectangle(BaseGeometry):
     """defining the rectanglr"""
 
     def __init__(self, width, height):
-        """making height and width private"""
+        """instantation"""
 
-        self.__width = 0
-        self.__height = 0
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
