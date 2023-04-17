a4e684c5962f46d98c449425c8dd153a94121598
hishammfaume <hishammfaume@gmail.com>
2023-04-17 18:08:24 +0000

    new rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,19 @@ from models.base import Base
 class Rectangle(Base):
     """private instances"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """iniialises rectangle
+         Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -45,6 +58,8 @@ class Rectangle(Base):
 
         @property
         def x(self):
+            """setting x coordinate"""
+
             return self.__x
 
         @x.setter
@@ -60,6 +75,8 @@ class Rectangle(Base):
         
         @property
         def y(self):
+            """stting y cordinate for rectangle"""
+
             return self.__y
 
         @y.setter
