4aef38ff43bd7c41bb805fafc93475c406ff8ecd
Hisham Mfaume <hisham.mfaume@ignitenursing.com>
2023-06-08 19:48:17 +0100

    tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,9 +5,13 @@
 def add_integer(a, b=98):
     """checking if a and b are integers or floats"""
 
-    if not isinstance(a, (int, float)):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
     """addition of two integer values"""
-    return a + b
+    return a+b
