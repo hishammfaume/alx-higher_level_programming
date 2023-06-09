896d3407aa12bf03424818194042aec4ffaf704b
Hisham Mfaume <hisham.mfaume@ignitenursing.com>
2023-06-09 16:09:24 +0100

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+ function that prints My name is <first name> <last name>
+"""
+
+def say_my_name(first_name, last_name=""):
+    if type(first_name) is not str:
+        raise TypeError("first name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("second name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
