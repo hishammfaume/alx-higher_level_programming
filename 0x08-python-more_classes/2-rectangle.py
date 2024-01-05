fcbae9179f6766a0e0a0dc88d253b225f88b94bf
hishammfaume <hishammfaume@gmail.com>
2024-01-05 16:54:33 +0300

    area and perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    defines a rectangle
+"""
+
+
+class Rectangle:
+    """Representing a rectangle"""
+    def __init__(self, width=0, height=0):
+        """initializing"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter for private instance"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """setter for heght"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    def area(self):
+        """area"""
+        area = self.__height * self.__width
+        return area
+    def perimeter(self):
+        """perimeter"""
+        peri = 2 * (self.__height + self.__width)
+        if self.__height == 0 or self.__width == 0:
+            peri = 0
+        return peri
